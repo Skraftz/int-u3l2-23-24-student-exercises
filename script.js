03abae18cf6df23e17a8b97aabcfb09cd433d83e
Skraftz <davekenkent@gmail.com>
2024-06-11 11:23:33 -0400

    I'm committed

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // 1. Take a look at these variables - how are they different than what we've seen?
 let paragraph = document.querySelector("p");
 let img = document.querySelector("img");
-
+// They direct to a specific type of line
 
 // 2. Remove the comment backslashes from the code below. What happened?
-// paragraph.innerHTML = "Here's a photo of a bear.";
-// img.src = "https://bearwise.org/wp-content/uploads/2022/09/the-over-the-shoulder-pose-scaled-736x414.jpg"; 
+paragraph.innerHTML = "Here's a photo of an ugly bear.";
+img.src = "https://bearwise.org/wp-content/uploads/2022/09/the-over-the-shoulder-pose-scaled-736x414.jpg"; 
